a4489a142177304d9c7bd52f18483579ba054bd6
s;rx <8214028+wlrsx@users.noreply.github.com>
2026-09-02 01:27:55 +0800

    Update dictionary.js

--- sub-store/scripts/dictionary.js
+++ sub-store/scripts/dictionary.js
@@ -129,7 +129,7 @@ const dict = {
           url: "https://cdn.jsdelivr.net/gh/wlrsx/proxy-rules-kit@refs/heads/main/rules/CustomProxy.yaml" }
       ],
       "TikTok-Bypass": [
-        { key: "tiktok_bypass", behavior: "classical", format: "yaml",
+        { key: "tiktok_bypass", behavior: "domain", format: "text",
           url: "https://cdn.jsdelivr.net/gh/wlrsx/proxy-rules-kit@refs/heads/main/rules/TikTok/TikTok-Bypass.yaml" }
       ],
       "TikTok": [
